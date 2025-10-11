174ec5741baa78b19d54cee08678177719740f0e
zhai changhao <changhao.zhai@shopee.com>
2025-10-11 15:37:53 +0800

    init

---
+++ dist/mini-react.js
@@ -0,0 +1,23 @@
+"use strict";
+const createElement = (type, props, ...children) => {
+    return {
+        type,
+        props: Object.assign(Object.assign({}, props), { children: children.map(child => {
+                const isTextNode = typeof child === 'string' || typeof child === 'number';
+                return isTextNode ? createTextNode(child) : child;
+            }) })
+    };
+};
+const createTextNode = (nodeValue) => {
+    return {
+        type: 'TEXT_NODE',
+        props: {
+            nodeValue,
+            children: [],
+        }
+    };
+};
+const MiniReact = {
+    createElement,
+};
+window.MiniReact = MiniReact;
